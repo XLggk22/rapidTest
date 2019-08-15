c598e5380118c38c05888a6756385906697c9cef
raobinghua <raobinghua@smyfinancial.com>
2019-08-15 11:20:52 +0800

    first commit

---
+++ src/main/java/my/test/gernate/code/rapid_Test/App.java
@@ -0,0 +1,13 @@
+package my.test.gernate.code.rapid_Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
